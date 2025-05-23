9a80a505b52fc31a59d028f7cd1e0672ef653d25
AhmedHussein222 <ahmdhussein222@gmail.com>
2025-05-23 15:29:37 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,21 +35,20 @@ class MyApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: S.delegate.supportedLocales,
-      home: CourseContentScreen(courseId: '1'),
-      // StreamBuilder<User?>(
-      //   stream: FirebaseAuth.instance.authStateChanges(),
-      //   builder: (context, snapshot) {
-      //     if (snapshot.connectionState == ConnectionState.waiting) {
-      //       return const Center(child: CircularProgressIndicator());
-      //     }
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-      //     if (snapshot.hasData) {
-      //       return const HomeScreen();
-      //     }
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
 
-      //     return const SplashScreen();
-      //   },
-      // ),
+          return const SplashScreen();
+        },
+      ),
      
       onGenerateRoute: (settings) {
         if (settings.name == '/feature') {
